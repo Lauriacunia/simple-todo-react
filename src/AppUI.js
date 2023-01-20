01510fe04b1e7fbeb3b934290a5517491e958660
Lauriacunia <lauriacunia@gmail.com>
2023-01-20 09:02:28 -0300

    implemento useContext

--- src/AppUI.js
+++ src/AppUI.js
@@ -8,14 +8,20 @@ import { CreateTodoButton } from "./components/CreateTodoButton";
 import { TodoContext } from "./utils/TodoContext";
 
 function AppUI() {
+  const {
+    serchedItem,
+    completeTodo,
+    uncompleteTodo,
+    deleteTodo,
+  } = React.useContext(TodoContext);
   return (
     <>
       <TodoCounter />
       <TodoSearch />
 
-      <TodoContext.Consumer>
+      {/* <TodoContext.Consumer> */}
         {/* Sintaxis de render props. Se envia un callback que recibe el valor del contexto */}
-        { ({ serchedItem, completeTodo, uncompleteTodo, deleteTodo }) => (
+        {/* { ({ serchedItem, completeTodo, uncompleteTodo, deleteTodo }) => ( */}
           <TodoList>
           {/*Accedo a la lista con props.children */}
           {serchedItem.map((todo) => (
@@ -35,8 +41,8 @@ function AppUI() {
             />
           ))}
         </TodoList>
-        )}
-      </TodoContext.Consumer>
+        {/* )} */}
+      {/* </TodoContext.Consumer> */}
 
       <CreateTodoButton />
     </>
